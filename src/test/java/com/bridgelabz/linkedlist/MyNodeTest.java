93a680e52628c252c03d14201dedab0a8dcb97ec
Mohit Shah <mohitshah7777@gmail.com>
2021-04-28 20:20:08 +0530

    Added: Simple Linked List has been created

--- src/test/java/com/bridgelabz/linkedlist/MyNodeTest.java
+++ src/test/java/com/bridgelabz/linkedlist/MyNodeTest.java
@@ -1,4 +1,18 @@
 package com.bridgelabz.linkedlist;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class MyNodeTest {
+
+    @Test
+    public void given3NumberWhenLinked_ShouldPassLinkedListTest() {
+        MyNode<Integer> myFirstNode = new MyNode<>(56);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(70);
+        myFirstNode.setNext(mySecondNode);
+        mySecondNode.setNext(myThirdNode);
+        boolean result = myFirstNode.getNext().equals(mySecondNode) && mySecondNode.getNext().equals(myThirdNode);
+        Assert.assertTrue(result);
+    }
 }
