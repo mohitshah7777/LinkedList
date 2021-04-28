d1442d05c149131d2d4e65202090f02ece5cf9c6
Mohit Shah <mohitshah7777@gmail.com>
2021-04-28 22:01:19 +0530

    Added: Delete last element from linked list

--- src/main/java/com/bridgelabz/linkedlist/MyLinkedList.java
+++ src/main/java/com/bridgelabz/linkedlist/MyLinkedList.java
@@ -48,6 +48,17 @@ public class MyLinkedList {
         return tempNode;
     }
 
+    //UC6 DELETE THE LAST ELEMENT
+    public INode popLast(){
+        INode tempNode = head;
+        while (!tempNode.getNext().equals(tail)) {
+            tempNode = tempNode.getNext();
+        }
+        this.tail = tempNode;
+        tempNode = null;
+        return tempNode;
+    }
+
     public void printMyNodes(){
         StringBuffer myNodes = new StringBuffer("My Nodes: ");
         INode tempNode = head;
--- src/test/java/com/bridgelabz/linkedlist/MyLinkedListTest.java
+++ src/test/java/com/bridgelabz/linkedlist/MyLinkedListTest.java
@@ -70,4 +70,21 @@ public class MyLinkedListTest {
                 myLinkedList.tail.equals(myThirdNode);
         Assert.assertTrue(result);
     }
+
+    @Test
+    public void given3Number_WhenDeletedLastElement_ShouldPassTheLinkedListTest() {
+        MyNode<Integer> myFirstNode = new MyNode<>(56);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(70);
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.append(myFirstNode);
+        myLinkedList.append(mySecondNode);
+        myLinkedList.append(myThirdNode);
+        myLinkedList.printMyNodes();
+        myLinkedList.popLast();
+        myLinkedList.printMyNodes();
+        boolean result = myLinkedList.head.equals(myFirstNode) &&
+                myLinkedList.tail.equals(mySecondNode);
+        Assert.assertTrue(result);
+    }
 }
